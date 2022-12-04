24cfc1451977d6ab452aedb5d0354c149096b564
Crisvc <117755400+Crisvc@users.noreply.github.com>
2022-12-03 23:25:19 -0500

    Attempt 1.

--- src/assigment2/Assigment2.java
+++ src/assigment2/Assigment2.java
@@ -1,10 +1,32 @@
 package assigment2;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assigment2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
+	public static void main(String [] args) {
+	
+ Scanner scanner = new Scanner(System.in);		
+ Random random = new Random();
+ int randomNumber = random.nextInt(100) + 1;
+ System.out.println("Please choose a number between 1 and 100.");
+ int playerGuess = scanner.nextInt();
+ 
+while (true) {
+if (playerGuess == randomNumber) {
+	System.out.println("YOU WIN!");
+} else if (playerGuess > randomNumber) {
+	System.out.println("Please choose a lower number:");
+} else {
+	System.out.println("Please choose a higher number:");
 }
+	 
+ }
+	
+	
+	
+}
+}
+
+	
