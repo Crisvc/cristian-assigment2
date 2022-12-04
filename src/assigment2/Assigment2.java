b3d1199e84459d2450b9cd4f637f2861f8a32ec6
Crisvc <117755400+Crisvc@users.noreply.github.com>
2022-12-04 14:56:38 -0500

    Final code.

--- src/assigment2/Assigment2.java
+++ src/assigment2/Assigment2.java
@@ -6,27 +6,49 @@ import java.util.Scanner;
 public class Assigment2 {
 	
 	public static void main(String [] args) {
+
+	// variables 
+		
+		int guess = -1;
+		int tries = 0;
+		boolean correct = false;
+		
+		// input
+		
+		Scanner keyboard = new Scanner(System.in);
+		Random rand = new Random();
+		int secretNum = rand.nextInt(100) + 1;
+		
+		System.out.println("Guess a number between 0 and 100:");
+		
+		
+		while (!correct) {
+			
+			guess = keyboard.nextInt();
+			tries++;
+			if (tries > 4) {
+				System.out.println("You lose, the number to guess was " + secretNum);
+				break;
+			}
+			
+			if (guess == secretNum) {
+				correct = true;
+				System.out.println("You WIN!");
+			}
+			else if (guess < secretNum) {
+				System.out.println("Please pick a Higher Number:");
+			}
+			else if (guess > secretNum) {
+				System.out.println("Please pick a Lower Number:");
+				
+			}
+			
+			
+		}
 	
- Scanner scanner = new Scanner(System.in);		
- Random random = new Random();
- int randomNumber = random.nextInt(100) + 1;
- System.out.println("Please choose a number between 1 and 100.");
- int playerGuess = scanner.nextInt();
- 
-while (true) {
-if (playerGuess == randomNumber) {
-	System.out.println("YOU WIN!");
-} else if (playerGuess > randomNumber) {
-	System.out.println("Please choose a lower number:");
-} else {
-	System.out.println("Please choose a higher number:");
-}
 	 
- }
 	
-	
-	
-}
+	}
 }
 
 	
